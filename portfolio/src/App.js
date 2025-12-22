0865e60a41b45966874b167f1514fc8f10ef6e2b
David Vincent <bahs.dvincent@gmail.com>
2025-12-22 14:54:38 -0500

    portfolio skeleton

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {motion} from "motion/react";
 import './App.css';
-
 function App() {
+  const homeRef = React.useRef(null);
+  const aboutRef = React.useRef(null);
+  const learningRef = React.useRef(null);
+  const projectsRef = React.useRef(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar">
+        <ul>
+          <li onClick={() => homeRef.current.scrollIntoView({ behavior: "smooth"})}>Home</li>
+          <li onClick={() => aboutRef.current.scrollIntoView({ behavior: "smooth"})}>About</li>
+          <li onClick={() => projectsRef.current.scrollIntoView({ behavior: "smooth"})}>Projects</li>
+          <li onClick={() => learningRef.current.scrollIntoView({ behavior: "smooth"})}>Learning</li>
+        </ul>
+      </nav>
+      <section ref={homeRef} className='section Home'>Home</section>
+      <section ref={aboutRef} className='section About'>About</section>
+      <section ref={projectsRef} className='section Projects'>Projects</section>
+      <section ref={learningRef} className='section Learning'>Learning</section>
     </div>
+    
   );
 }
 
